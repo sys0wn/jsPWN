75cbff0e2573e6a81ae0ba2dce4c401269390e2c
sys0wn <138518220+sys0wn@users.noreply.github.com>
2023-07-07 10:16:16 -0400

    Installation script for bash

--- installForBash.sh
+++ installForBash.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mv ../jsPWN ../jsPWNGitCanDelete
 mkdir ~/jsPWN
 cp requirements.txt ~/jsPWN/
@@ -6,5 +8,4 @@ cd ~/jsPWN
 python3 -m venv venv
 source ~/jsPWN/venv/bin/activate
 pip install -r requirements.txt
-echo 'alias jsPWN="python3 ~/jsPWN/jsPWN.py"' >> ~/.zshrc
 echo 'alias jsPWN="python3 ~/jsPWN/jsPWN.py"' >> ~/.bashrc
