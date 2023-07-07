8ba89454090c8e2fc49fa6a1d4ac853d4890b057
sys0wn <138518220+sys0wn@users.noreply.github.com>
2023-07-07 09:59:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-mkdir ~/jsPWN
+mv ../jsPWN ../jsPWNGitCanDelete
+cp requirements.txt ~/jsPWN/
+cp jsPWN.py ~/jsPWN/
 cd ~/jsPWN
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 echo 'alias jsPWN="python3 ~/jsPWN/jsPWN.py"' > ~/.zshrc
 echo 'alias jsPWN="python3 ~/jsPWN/jsPWN.py"' > ~/.bashrc
+rm -rf ~/jsPWNGitCanDelete
