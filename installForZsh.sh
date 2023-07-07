38b4c2dd1bc26655c46848497ed989bfb6063451
sys0wn <138518220+sys0wn@users.noreply.github.com>
2023-07-07 10:17:31 -0400

    Installation script for ZSH

---
+++ installForZsh.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+mv ../jsPWN ../jsPWNGitCanDelete
+mkdir ~/jsPWN
+cp requirements.txt ~/jsPWN/
+cp jsPWN.py ~/jsPWN/
+cd ~/jsPWN
+python3 -m venv venv
+source ~/jsPWN/venv/bin/activate
+pip install -r requirements.txt
+echo 'alias jsPWN="python3 ~/jsPWN/jsPWN.py"' >> ~/.zshrc
